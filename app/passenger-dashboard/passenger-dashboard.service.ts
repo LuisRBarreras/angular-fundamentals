3305823399fc38259320d9791a3aa0ac98e9894c
Luis Barreras <luis.barreras@unosquare.com>
2020-04-03 05:56:47 -0700

    Unit Services, Http and Observable lesson 3 http obsersables get

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,34 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import { Passenger } from './models/passenger.interface';
-import { Http } from '@angular/http';
+
+const PASSENGER_API: string = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
   constructor(private http: Http) {}
 
-  getPassengers(): Passenger[] {
-    return [
-      {
-        id: 1,
-        fullname: 'Stephen',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: null
-      },
-      {
-        id: 2,
-        fullname: 'Rose',
-        checkedIn: false,
-        checkInDate: null,
-        children: [{ name: 'Nicolas', age: 5 }]
-      },
-      {
-        id: 3,
-        fullname: 'Luis',
-        checkedIn: true,
-        checkInDate: null,
-        children: [{ name: 'Ted', age: 15 }]
-      }
-    ];
+  getPassengers(): Observable<Passenger[]> {
+    return this.http
+      .get(PASSENGER_API) //
+      .map((response: Response) => response.json());
   }
 }
