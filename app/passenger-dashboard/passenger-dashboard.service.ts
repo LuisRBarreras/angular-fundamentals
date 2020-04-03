e6dcbb23e3ac7beb636d4b8f79bfda32c31612be
Luis Barreras <luis.barreras@unosquare.com>
2020-04-03 07:42:19 -0700

    Unit 'Services, Http and Observable' Lesson 5 'heaaders request options'

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -12,9 +12,31 @@ const PASSENGER_API: string = '/api/passengers';
 export class PassengerDashboardService {
   constructor(private http: Http) {}
 
+
+
   getPassengers(): Observable<Passenger[]> {
     return this.http
       .get(PASSENGER_API) //
+      .map((response: Response) => response.json());
+  }
+
+  updatePassenger(passenger: Passenger): Observable<Passenger> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    let options = new RequestOptions({
+      headers
+    });
+
+    return this.http
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger, options) //
+      .map((response: Response) => response.json());
+  }
+
+  removePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+      .delete(`${PASSENGER_API}/${passenger.id}`) //
       .map((response: Response) => response.json());
   }
 }
