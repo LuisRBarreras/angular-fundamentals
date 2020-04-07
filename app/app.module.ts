41c2b90a332c05b1f0c1b5ad52869836bca7ba5f
Luis Barreras <lrbs0190@gmail.com>
2020-04-07 07:19:53 -0700

    Unit 'Component Routing' Lesson 3 'Wildcard notfound'

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDahboardModule }  from './passenger-dashboard/passenger-dashboard.module';
 
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     //angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
 
     //custom modules
     PassengerDahboardModule,
